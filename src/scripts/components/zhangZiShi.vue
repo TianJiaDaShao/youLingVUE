63785358c5c91964ad73f63936535df6424dd3a1
chinawzc <wang909208@163.com>
2016-10-22 16:52:10 +0800

    wang

--- src/scripts/components/zhangZiShi.vue
+++ src/scripts/components/zhangZiShi.vue
@@ -1,3 +1,15 @@
 <template>
-    132
+    <div class="gesture">
+        <section id="index-scroll">
+            <ul>
+                <li v-for="l in list" v-link="{name:'detail',params:{id:l.id}}" @click="clc">
+                    <img v-bind:src="l.img" alt="">
+                    <i>{{l.title}}</i>
+                    <p>{{l.detail}}</p>
+                    <b><span>涨姿势</span><span>{{l.type}}</span></b>
+                    <b><span>作者：领哥</span><span><img src="./images/md_view_xs.png" alt="">{{l.num}}</span><span>{{l.time}}</span></b>
+                </li>
+            </ul>
+        </section>
+    </div>
 </template>
