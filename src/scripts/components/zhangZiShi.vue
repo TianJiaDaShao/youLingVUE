b146baf3c72170e0e1409cbd90cb721b2d7a4a82
chinawzc <wang909208@163.com>
2016-10-23 16:07:57 +0800

    zhangzishi

--- src/scripts/components/zhangZiShi.vue
+++ src/scripts/components/zhangZiShi.vue
@@ -1,15 +1,48 @@
 <template>
     <div class="gesture">
+        <header>
+            涨姿势
+        </header>
         <section id="index-scroll">
             <ul>
-                <li v-for="l in list" v-link="{name:'detail',params:{id:l.id}}" @click="clc">
+                <li v-for="l in list" @click="clc" v-link="{path:'/zhangdetail'}">
                     <img v-bind:src="l.img" alt="">
                     <i>{{l.title}}</i>
-                    <p>{{l.detail}}</p>
-                    <b><span>涨姿势</span><span>{{l.type}}</span></b>
-                    <b><span>作者：领哥</span><span><img src="./images/md_view_xs.png" alt="">{{l.num}}</span><span>{{l.time}}</span></b>
+                    <p>{{l.det}}</p>
+                    <b class="b1"><span>涨姿势</span>&nbsp;&nbsp;&nbsp;&nbsp;<span>{{l.type}}</span></b>
+                    <b class="b2"><span>作者：领哥</span><span><img src="/images/md_view_xs.png" alt="">&nbsp;&nbsp;{{l.num}}</span><span>{{l.time}}</span></b>
                 </li>
             </ul>
         </section>
     </div>
 </template>
+<script>
+    var Vue = require('../libs/vue.js');
+    var VueResource = require('../libs/vue-resource.js');
+    Vue.use(VueResource);
+
+    export default {
+        data() {
+                return {
+                    list: []
+                }
+            },
+            ready: function() {
+                var that = this;
+                this.$http.get('/mock/zhangZiShi.json')
+                    .then((res) => {
+                        this.list = res.data.data;
+                        setTimeout(function() {
+                            new IScroll('#index-scroll', {
+                                click:true
+                            });
+                        }, 500);
+                    })
+            },
+            methods:{
+                clc(){
+                    console.log(1)
+                }
+            }
+    }
+</script>
